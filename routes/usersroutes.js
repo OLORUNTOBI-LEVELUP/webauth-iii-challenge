260a5cac4c0fe44f1d7038231fe9a4f206d1494d
oloruntobi <awoderuoloruntobi@gmail.com>
2019-07-24 13:52:10 +0100

    Set up post routes

---
+++ routes/usersroutes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const db = require('../users/userModel')
+
+
+router.post('/register', (req, res) => {
+    let user = req.body;
+    const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
+    user.password = hash;
+  
+    db.add(user)
+      .then(saved => {
+        res.status(201).json(saved);
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  });
+  
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const server = express();
 const port = process.env.PORT || 3000
+const userRoutes = require('./routes/usersroutes')
 
 server.use(express.json());
-
+server.use('/auth/api', userRoutes)
 
 
 server.listen(port, () => console.log(`listening on port ${port}`) )
